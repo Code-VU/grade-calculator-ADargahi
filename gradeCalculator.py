7fb0bd5c18db13324fd6ed6de4f3cba6e1d8e451
Abbas Dargahi <adargahi97@gmail.com>
2022-01-26 16:58:52 -0600

    Assignment 3.3

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,7 +3,27 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+    grade = ""
+    hrs = input("Enter score: ")
+
+    try:
+        hrs = float(hrs)
+        if hrs >=0.9:
+            grade = "A"
+        elif hrs >=0.8:
+            grade = "B"
+        elif hrs >=0.7:
+            grade = "C"
+        elif hrs >=0.6:
+            grade = "D"
+        else:
+            grade = "Bad score"
+
+    except:
+        print("Bad score")
+
+    print(grade)
+
 
     # end assignment
 
@@ -11,4 +31,4 @@ def calculateGrade():
 ## uncomment calculateGrade() and run > python calculateGrade.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-## calculateGrade()
+#calculateGrade()
